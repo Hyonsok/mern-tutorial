b79e8d65b65a21c95ddcfe5ce0edf543ee614fce
Hyonsok <46020558+Hyonsok@users.noreply.github.com>
2023-02-11 09:56:40 -0800

    signing up & hasing passwords

---
+++ backend/models/userModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+//to create schema
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+// static signup method
+// important!!! arrow doesn't work if 'this' is used!
+userSchema.statics.signup = async function(email, password) {
+
+    const exists = await this.findOne({ email })
+
+    if (exists) {
+        throw Error('Email already in use')
+    }
+
+    // use bcrypt 
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({ email, password: hash })
+
+    return user
+}
+
+module.exports = mongoose.model('User', userSchema)
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
+const userRoutes = require('./routes/user');
 require('dotenv').config();
 
 // express app
@@ -16,6 +17,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/workouts', workoutRoutes);
+app.use('/api/user', userRoutes);
 
 //connect to db
 mongoose.set("strictQuery", false);
